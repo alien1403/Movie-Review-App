8eb06170b54892234e4dfba669c614c35dbbc8b4
alien1403 <hanghicelrazvanmihai@gmail.com>
2023-09-14 16:23:06 +0300

    Add frontend files

---
+++ frontend/src/components/trailer/Trailer.js
@@ -0,0 +1,20 @@
+import { useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import "./Trailer.css"
+
+import React from 'react'
+
+const Trailer = () => {
+
+    let params = useParams()
+    let key = params.ytTrailerId
+
+  return (
+    <div className="reac-player-component">
+        {(key!=null)?<ReactPlayer controls="true" playing={true} url={`https://www.youtube.com/watch?v=${key}`}
+         width='100%' height='100%'/>:null}
+    </div>
+  )
+}
+
+export default Trailer
